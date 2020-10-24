89e69642a0b6b105e3b0a6e049591ad85f08e801
FrutlesPoent <frutlespoent@gmail.com>
2020-10-24 20:33:09 +0700

    Add logic

--- Sequencer/SequencerPlay.java
+++ Sequencer/SequencerPlay.java
@@ -1,35 +1,83 @@
 package Sequencer;
 
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-public class SequencerPlay implements ControllerEventListener {
+abstract public class SequencerPlay {
+    private static Track track;
+    private static Sequence seq;
+    private static int[] instrumentNames;
+    private static Sequencer sequencer;
+    private static ArrayList<JCheckBox> checkBoxesList;
 
-    public void sequencerPlay(myDraw draw){
+    public static void sequencerPlay(int[] instrumentNames, ArrayList<JCheckBox> checkBoxesList){
         try {
-            Sequencer sequencer = MidiSystem.getSequencer();
+            SequencerPlay.checkBoxesList = checkBoxesList;
+            SequencerPlay.instrumentNames = instrumentNames;
+            sequencer = MidiSystem.getSequencer();
             sequencer.open();
-            sequencer.addControllerEventListener(draw, new int[] {127});
-            Sequence seq = new Sequence(Sequence.PPQ,4);
-            Track track = seq.createTrack();
-            MidiEvent even = null;
-            int r = 0;
-            for (int i = 5; i < 60; i +=4 ) {
-                r = (int) ((Math.random() * 50) + 1);
-                track.add(Message.createMessage(144, 1, r, 100, i));
-                track.add(Message.createMessage(176, 1, 127, 0, i));
-                track.add(Message.createMessage(128, 1,r, 100, i + 2));
-            }
-            sequencer.setSequence(seq);
+            seq = new Sequence(Sequence.PPQ,4);
+            track = seq.createTrack();
             sequencer.setTempoInBPM(120);
-            sequencer.start();
         } catch(Exception ex) {
             ex.printStackTrace();
         }
-
     }
 
-    @Override
-    public void controlChange(ShortMessage event) {
-            System.out.println("Ля");
+    public static void buildTrackAndStart() throws InvalidMidiDataException {
+        int[] trackList = null; // to save for every instrument (16)
+        seq.deleteTrack(track); // deleting old one;
+        track = seq.createTrack(); // creating new one
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+            int key = SequencerPlay.instrumentNames[i];
+            for (int j = 0; j < 16; j++) {
+                JCheckBox jCheckBox = (JCheckBox) SequencerPlay.checkBoxesList.get(j + (16 * i));
+                if (jCheckBox.isSelected()) {
+                    trackList[j] = key; //if "yes" then we place key into massive which represents tact;
+                } else {
+                    trackList[j] = 0; // instrument should not play if he equals zero;
+                }
+            }
+            makeTracks(trackList);
+            track.add(Message.createMessage(176, 1, 127, 0, 16));
+        }
+        track.add(Message.createMessage(192,9,1,0, 15)); // music could not complete all 15 tact's, so we have to be sure;
+        try {
+            sequencer.setSequence(seq);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY); // infinite cycle;
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void makeTracks(int[] list) throws InvalidMidiDataException {
+        for (int i = 0; i < 16; i++) {
+            int key = list[i];
+            if (key != 0) {
+                track.add(Message.createMessage(144, 9, key, 100, i));
+                track.add(Message.createMessage(128,9, key, 100, i + 1));
+            }
+        }
+    }
+
+    public static void stopPlay() {
+        sequencer.stop();
+    }
+
+    public static void increasePlay() {
+        float tempFactor = sequencer.getTempoFactor();
+        sequencer.setTempoFactor((float) (tempFactor * 1.03)); // we can increase by 3%;
+    }
+
+    public static void downPlay() {
+        float tempFactor = sequencer.getTempoFactor();
+        sequencer.setTempoFactor((float) (tempFactor * .97)); // decrease by 3%;
     }
 }
