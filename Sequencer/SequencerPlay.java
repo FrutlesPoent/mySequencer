50237c2faa4d08536e5f582af35a094368d83313
FrutlesPoent <frutlespoent@gmail.com>
2020-11-12 20:14:43 +0700

    remove all static and move func createMessage

--- Sequencer/SequencerPlay.java
+++ Sequencer/SequencerPlay.java
@@ -7,38 +7,39 @@ import java.io.*;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
-abstract public class SequencerPlay {
-    private static Track track;
-    private static Sequence seq;
-    private static int[] instrumentNames;
-    private static Sequencer sequencer;
-    private static ArrayList<JCheckBox> checkBoxesList;
-    private static  final int numberOfFlags = 256;
+public class SequencerPlay {
 
-    public static void sequencerPlay(int[] instrumentNames, ArrayList<JCheckBox> checkBoxesList){
+    private Track track;
+    private Sequence seq;
+    private int[] instrumentNames;
+    private Sequencer sequencer;
+    private ArrayList<JCheckBox> checkBoxesList;
+    private final int numberOfFlags = 256;
+
+    public void sequencerPlay(int[] instrumentNames, ArrayList<JCheckBox> checkBoxesList){
         try {
-            SequencerPlay.checkBoxesList = checkBoxesList;
-            SequencerPlay.instrumentNames = instrumentNames;
-            sequencer = MidiSystem.getSequencer();
-            sequencer.open();
-            seq = new Sequence(Sequence.PPQ,4);
-            track = seq.createTrack();
-            sequencer.setTempoInBPM(120);
+            this.checkBoxesList = checkBoxesList;
+            this.instrumentNames = instrumentNames;
+            this.sequencer = MidiSystem.getSequencer();
+            this.sequencer.open();
+            this.seq = new Sequence(Sequence.PPQ,4);
+            this.track = seq.createTrack();
+            this.sequencer.setTempoInBPM(120);
         } catch(Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public static void buildTrackAndStart() throws InvalidMidiDataException {
+    public void buildTrackAndStart() throws InvalidMidiDataException {
         int[] trackList = null; // to save for every instrument (16)
-        seq.deleteTrack(track); // deleting old one;
-        track = seq.createTrack(); // creating new one
+        this.seq.deleteTrack(track); // deleting old one;
+        this.track = this.seq.createTrack(); // creating new one
 
         for (int i = 0; i < 16; i++) {
             trackList = new int[16];
-            int key = SequencerPlay.instrumentNames[i];
+            int key = this.instrumentNames[i];
             for (int j = 0; j < 16; j++) {
-                JCheckBox jCheckBox = (JCheckBox) SequencerPlay.checkBoxesList.get(j + (16 * i));
+                JCheckBox jCheckBox = (JCheckBox) this.checkBoxesList.get(j + (16 * i));
                 if (jCheckBox.isSelected()) {
                     trackList[j] = key; //if "yes" then we place key into massive which represents tact;
                 } else {
@@ -46,9 +47,9 @@ abstract public class SequencerPlay {
                 }
             }
             makeTracks(trackList);
-            track.add(Message.createMessage(176, 1, 127, 0, 16));
+            this.track.add(createMessage(176, 1, 127, 0, 16));
         }
-        track.add(Message.createMessage(192,9,1,0, 15)); // music could not complete all 15 tact's, so we have to be sure;
+        this.track.add(createMessage(192,9,1,0, 15)); // music could not complete all 15 tact's, so we have to be sure;
         try {
             sequencer.setSequence(seq);
             sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY); // infinite cycle;
@@ -59,31 +60,31 @@ abstract public class SequencerPlay {
         }
     }
 
-    public static void makeTracks(int[] list) throws InvalidMidiDataException {
+    private void makeTracks(int[] list) throws InvalidMidiDataException {
         for (int i = 0; i < 16; i++) {
             int key = list[i];
             if (key != 0) {
-                track.add(Message.createMessage(144, 9, key, 100, i));
-                track.add(Message.createMessage(128,9, key, 100, i + 1));
+                this.track.add(createMessage(144, 9, key, 100, i));
+                this.track.add(createMessage(128,9, key, 100, i + 1));
             }
         }
     }
 
-    public static void stopPlay() {
+    public void stopPlay() {
         sequencer.stop();
     }
 
-    public static void increasePlay() {
+    public void increasePlay() {
         float tempFactor = sequencer.getTempoFactor();
         sequencer.setTempoFactor((float) (tempFactor * 1.03)); // increase by 3%;
     }
 
-    public static void downPlay() {
+    public void downPlay() {
         float tempFactor = sequencer.getTempoFactor();
         sequencer.setTempoFactor((float) (tempFactor * .97)); // decrease by 3%;
     }
 
-    public static void restoreMusic(String musicName) throws InvalidMidiDataException {
+    public void restoreMusic(String musicName) throws InvalidMidiDataException {
         stopPlay();
 
         String impl = ".ser";
@@ -112,12 +113,12 @@ abstract public class SequencerPlay {
         buildTrackAndStart();
     }
 
-    public static void clearTrack() {
+    public void clearTrack() {
         for (int i = 0; i < numberOfFlags; i++)
             checkBoxesList.get(i).setSelected(false);
     }
 
-    public static void saveMusic(String musicName) {
+    public void saveMusic(String musicName) {
         stopPlay();
         String impl = ".ser";
         if (musicName == null){
@@ -142,5 +143,12 @@ abstract public class SequencerPlay {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private MidiEvent createMessage(int typeMessage, int channel, int one, int two, int moment) throws InvalidMidiDataException{
+        ShortMessage message = new ShortMessage();
+        message.setMessage(typeMessage,channel,one , two);
+        MidiEvent noteOn = new MidiEvent(message, moment);
+        return noteOn;
     }
 }
